df32fe4faff9a95c64a4feb59ba53546648bfe11
Xinhao Yuan <xinhaoyuan@gmail.com>
2016-11-05 13:18:33 -0400

    .

--- python/simpleon-py/parse_file.py
+++ python/simpleon-py/parse_file.py
@@ -4,7 +4,6 @@ def parse_file(f, parser):
         try:
             parser.parse_line(line.rstrip("\r\n"))
         except Exception as e:
-            print(e)
             raise Exception("Parser exception at line {0}: {1}".format(line_num, str(e)))
         line_num += 1
     return parser.extract()
