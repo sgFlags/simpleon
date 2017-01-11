cf1578a8d7f492e6a954b7216b7c75c4a62ba139
Xinhao Yuan <xinhaoyuan@gmail.com>
2017-01-10 23:58:55 -0500

    bump version

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name = 'simpleon',
-      version ='0.1.0',
+      version ='0.2.0',
       description = "SimpleON (Simple Object Notation) format decoder",
       author = "Xinhao Yuan",
       author_email = "xinhaoyuan@gmail.com",
